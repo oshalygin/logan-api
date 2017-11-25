a9632de212b30b3ecb56c0702cd076dbdd2808a1
Oleg Shalygin <oshalygin@gmail.com>
2017-11-24 21:28:18 -0800

    Add app configuration loader
    - The config.go file loads the configuration files from app.yaml
      or errors.yaml and properly sets them on the config object.
    - Validation is unnecessary at this time.

---
+++ app/config.go
@@ -0,0 +1,50 @@
+package app
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+// Config stores the application-wide configurations
+var Config appConfig
+
+type appConfig struct {
+	// the path to the error message file. Defaults to "config/errors.yaml"
+	ErrorFile string `mapstructure:"error_file"`
+	// the server port. Defaults to 8080
+	ServerPort int `mapstructure:"server_port"`
+	// the data source name (DSN) for connecting to the database. required.
+	DSN string `mapstructure:"dsn"`
+	// the signing method for JWT. Defaults to "HS256"
+	JWTSigningMethod string `mapstructure:"jwt_signing_method"`
+	// JWT signing key. required.
+	JWTSigningKey string `mapstructure:"jwt_signing_key"`
+	// JWT verification key. required.
+	JWTVerificationKey string `mapstructure:"jwt_verification_key"`
+}
+
+// LoadConfig loads configuration from the given list of paths and populates it into the Config variable
+func LoadConfig(configPaths ...string) error {
+	v := viper.New()
+	v.SetConfigFile("app")
+	v.SetConfigType("yaml")
+	v.SetEnvPrefix("restful")
+	v.AutomaticEnv()
+
+	v.SetDefault("error_file", "config/errors.yaml")
+	v.SetDefault("server_port", 8080)
+	v.SetDefault("jwt_signing_method", "HS256")
+
+	for _, path := range configPaths {
+		v.AddConfigPath(path)
+	}
+
+	if err := v.ReadInConfig(); err != nil {
+		return fmt.Errorf("Failed to read the configuration file, %s", err)
+	}
+
+	if err := v.Unmarshal(&Config); err != nil {
+		return err
+	}
+}
